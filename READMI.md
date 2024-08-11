ee291cf638865136f1f335b58ea5fe6f1e2490e6
DzmitrySiarheyeu <dzmitry.siarheyeu@gmail.com>
2024-08-11 12:43:15 +0300

    init project

---
+++ READMI.md
@@ -0,0 +1,3 @@
+<h2 align="center">Messenger_server by Django</h2>
+
+Мессендер на Django Rest Framework.
--- src/profiles/models.py
+++ src/profiles/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 
 # Create your models here.
-class UserProf(AbstractUser):
+class User_Profiles(AbstractBaseUser):
     middle_name = models.CharField(max_length=50)
     first_login = models.DateTimeField(null=True)
     phone = models.CharField(max_length=14)
-    avatar = models.ImageField(apload_to='user/avatar/', blank=True, null=True)
+    avatar = models.ImageField(upload_to='user/avatar/', blank=True, null=True)
